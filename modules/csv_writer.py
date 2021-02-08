551e8e41aed932b965f41b51eff9ce08f86eebf2
Joshua Fogus <josh.fogus@gmail.com>
2021-02-08 13:47:07 -0800

    Implemented the csv writer.

--- modules/csv_writer.py
+++ modules/csv_writer.py
@@ -2,9 +2,25 @@
 # Last Modified:  February 7, 2021
 # Description:  A function which writes passed data to a csv.
 
+import csv
 
-def csv_writer():
-    pass
+
+def csv_writer(data, file_path=None):
+    """ Accepts an array of data, which should have length 3 and an optional
+        file path to which the data should be written. """
+
+    # Default file path
+    if file_path is None:
+        file_path = "../test_input.csv"
+
+    headers = ["input_item_type", "input_item_category", "input_number_to_generate"]
+
+    with open(file_path, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+
+        writer.writerow(headers)
+        # All data must be strings to be written
+        writer.writerow([str(value) for value in data])
 
 
 if __name__ == "__main__":
