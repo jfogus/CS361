da1122d8a99a907565a21caceb047dee7008e424
Joshua Fogus <josh.fogus@gmail.com>
2021-02-07 19:24:31 -0800

    Added unit tests

---
+++ unittests/csv_reader_tester.py
@@ -0,0 +1,13 @@
+# Name:  Joshua Fogus
+# Last Modified:  February 7, 2021
+# Description:  Unit tests for the csv reader.
+
+import unittest
+
+
+class CsvReaderTester(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ unittests/toy_list_tester.py
@@ -0,0 +1,13 @@
+# Name:  Joshua Fogus
+# Last Modified:  February 7, 2021
+# Description:  Unit tests for the toy list.
+
+import unittest
+
+
+class ToyListTester(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
