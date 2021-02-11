6b4faa0b7589e937d0e977b5744a5ef47064f4b6
Joshua Fogus <josh.fogus@gmail.com>
2021-02-11 11:37:47 -0800

    Refactor and initial implementation

--- modules/csv_writer.py
+++ modules/csv_writer.py
@@ -1,26 +1,21 @@
 # Name:  Joshua Fogus
-# Last Modified:  February 7, 2021
-# Description:  A function which writes passed data to a csv.
+# Last Modified:  February 11, 2021
+# Description:  A function which writes headers and data to a csv.
 
 import csv
 
 
-def csv_writer(data, file_path=None):
+def csv_writer(headers, data, file_path):
     """ Accepts an array of data, which should have length 3 and an optional
         file path to which the data should be written. """
-
-    # Default file path
-    if file_path is None:
-        file_path = "../test_input.csv"
-
-    headers = ["input_item_type", "input_item_category", "input_number_to_generate"]
 
     with open(file_path, 'w') as csv_file:
         writer = csv.writer(csv_file)
 
         writer.writerow(headers)
         # All data must be strings to be written
-        writer.writerow([str(value) for value in data])
+        for toy in data:
+            writer.writerow([str(attr) for attr in toy])
 
 
 if __name__ == "__main__":
--- modules/data_reader.py
+++ modules/data_reader.py
@@ -1,19 +1,23 @@
 # Name:  Joshua Fogus
-# Last Modified:  February 7, 2021
+# Last Modified:  February 10, 2021
 # Description:  A function which reads csv data from a file.
 
 import csv
 
 
-def csv_reader(file_path=None):
+def data_reader(file_path=None):
     """ Receives a path to a csv file and returns an array of
         object with keys matching the headers. """
 
-    # Default file path
-    if file_path is None:
-        file_path = "../sample_data.csv"
+    # The desired columns to be read
+    read_columns = [
+        "uniq_id",
+        "product_name",
+        "number_of_reviews",
+        "average_review_rating",
+        "amazon_category_and_sub_category"
+    ]
 
-    # This will be the output
     data_arr = []
 
     with open(file_path, 'r') as csv_file:
@@ -21,14 +25,15 @@ def csv_reader(file_path=None):
 
         # First row of the csv is headers
         headers = next(file_reader)
-        # This will be added to the output
-        data_obj = {}
 
         for line in file_reader:
-            for i, key in enumerate(headers):
+            # This will be added to the output
+            data_obj = {}
+
+            for i, field in enumerate(headers):
                 # Only specific columns returned
-                if key in ["uniq_id", "number_of_reviews", "average_review_rating", "amazon_category_and_sub_category"]:
-                    data_obj[key] = format_data(key, line[i])
+                if field in read_columns:
+                    data_obj[field] = format_data(field, line[i])
 
             data_arr.append(data_obj)
 
@@ -39,8 +44,8 @@ def format_data(header, content):
     """ Accepts a type of data as a string and content as a string.
         Returns a formatted version of content based on the type. """
 
-    # uniq_id does not need processed and remains a string
-    if header == "uniq_id":
+    # uniq_id and product name do not need processed and remain strings
+    if header == "uniq_id" or header == "product_name":
         return content
 
     # Number of reviews comes in as a string and returns as an integer
@@ -48,7 +53,7 @@ def format_data(header, content):
         try:
             return int(content.replace(",", ""))
         except ValueError:
-            return content
+            return 0
 
     # Number of reviews comes in as a string sentence and returns as
     # a simple integer
@@ -56,7 +61,7 @@ def format_data(header, content):
         try:
             return float(content.split(" ")[0])
         except ValueError:
-            return content
+            return 0.0
 
     # Amazon category and sub category comes in as a string separated
     # by > symbols and returns as an array
